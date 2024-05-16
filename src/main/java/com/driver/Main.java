14a21f83de70d915916e6d510f41bc9152fd9665
ASADANSH <ashish-sanjay.sadanshiv@capgemini.com>
2024-05-16 15:30:15 +0530

    Attempt 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,7 @@
 package com.driver;
 
 public class Main {
-//  public static void main(String[] args) {
-//    Pizza dp = new DeluxePizza(true);
-//    System.out.println(dp.getBasePrize());
-////    dp.addExtraToppings();
-////    dp.addExtraCheese();
-//    dp.addTakeaway();
-//    System.out.println(dp.getBill());
-//    
-//    Pizza dvp = new DeluxePizza(false);
-//    System.out.println(dvp.getPrice());
-////    dp.addExtraToppings();
-////    dp.addExtraCheese();
-//    dvp.addTakeaway();
-//    System.out.println(dvp.getBill());
-//
-//    Pizza p = new Pizza(true);
-//    p.addExtraToppings();
-//    p.addExtraCheese();
-//    p.addExtraToppings();
-//    System.out.println(p.getBill());
-//  }
+
 	
 	
 	public static void main(String args[]) {
